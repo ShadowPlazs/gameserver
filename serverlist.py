084c20fcf73b60f1360417c23ceb0f0b51a92bc3
DarkBYT153#4092 <91132683+ShadowPlazs@users.noreply.github.com>
2022-01-15 13:36:41 +0800

    “Serverlist.

---
+++ serverlist.py
@@ -0,0 +1,23 @@
+import servers
+from servers import backup
+
+def on_server(detected):
+  if new.server(detected):
+    reload.servers("reload")
+    action.showserver(all)
+    
+def if.on_detected(api_err):
+  if server.error(detected):
+    print('An unexpected error has occured in the module "Api". Please restart the application to continue your gameplay on a server.')
+    reboot.server(if):
+      if server.unstable():
+        reboot.server(as):
+          cancel
+          
+ def on_error():
+  if system.error(detected):
+    re.system(re.boot)
+  else:
+    action.cancel(action)
+  elif
+    action.crash(action.i)
